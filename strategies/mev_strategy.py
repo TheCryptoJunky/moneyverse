aaa8e034e2d292ff1ea5b58f726507003b38b8a9
TheCryptoJunky <thecryptojunky@gmail.com>
2024-10-28 13:46:16 -0400

    Update mev_strategy.py

--- strategies/mev_strategy.py
+++ strategies/mev_strategy.py
@@ -2,8 +2,15 @@
 
 import numpy as np
 from typing import List, Dict, Union
-from .utils import calculate_profit
 from src.managers.wallet_swarm import WalletSwarm
+from .moving_average_crossover_strategy import MovingAverageCrossoverStrategy
+from .multi_exchange_arbitrage_bot import MultiExchangeArbitrageBot
+from .revenge_bot import RevengeBot
+from .sniper_bot import SniperBot
+from .statistical_arbitrage_bot import StatisticalArbitrageBot
+from .triangle_arbitrage import TriangleArbitrageBot
+from .liquidity_drain_bot import LiquidityDrainBot
+from .liquidity_provision_arbitrage_bot import LiquidityProvisionArbitrageBot
 
 class MEVStrategy:
     """
@@ -22,82 +29,109 @@ class MEVStrategy:
 class Arbitrage(MEVStrategy):
     def __init__(self, wallet_swarm: WalletSwarm):
         super().__init__(wallet_swarm)
-        self.exchanges = ['Uniswap', 'SushiSwap', 'Curve']
+        self.bot = MultiExchangeArbitrageBot()
 
-    def identify_opportunities(self, market_data: Dict[str, float]) -> List[Dict[str, float]]:
-        opportunities = []
-        for exchange1 in self.exchanges:
-            for exchange2 in self.exchanges:
-                if exchange1 != exchange2:
-                    price1 = market_data[exchange1]
-                    price2 = market_data[exchange2]
-                    if price1 < price2:
-                        opportunities.append({
-                            'exchange1': exchange1,
-                            'exchange2': exchange2,
-                            'price1': price1,
-                            'price2': price2
-                        })
-        return opportunities
+    def identify_opportunities(self, data):
+        # Utilize the specific arbitrage bot’s capabilities
+        return self.bot.fetch_market_data()
 
-    def execute(self, opportunity: Dict[str, float]) -> float:
-        amount = 100  # Example fixed amount
-        profit = calculate_profit(opportunity['price1'], opportunity['price2'], amount)
-        return profit
+    def execute(self, opportunity):
+        return self.bot.execute_trade(opportunity)
 
 
-class FrontRunning(MEVStrategy):
-    def __init__(self, wallet_swarm: WalletSwarm, transaction_threshold: float = 1000):
+class MovingAverageCrossover(MEVStrategy):
+    def __init__(self, wallet_swarm: WalletSwarm):
         super().__init__(wallet_swarm)
-        self.transaction_threshold = transaction_threshold
+        self.strategy = MovingAverageCrossoverStrategy()
 
-    def identify_opportunities(self, transaction_data: List[Dict[str, float]]) -> List[Dict[str, float]]:
-        return [
-            {'transaction_hash': tx['hash'], 'value': tx['value']}
-            for tx in transaction_data if tx['value'] > self.transaction_threshold
-        ]
+    def identify_opportunities(self, data):
+        return self.strategy.generate_signals(data)
 
-    def execute(self, opportunity: Dict[str, float]) -> float:
-        profit = opportunity['value'] * 0.01  # Assume 1% profit
-        return profit
+    def execute(self, opportunity):
+        # Process signal to trade
+        return opportunity
 
 
-class BackRunning(MEVStrategy):
-    def __init__(self, wallet_swarm: WalletSwarm, transaction_threshold: float = 1000):
+class RevengeTrading(MEVStrategy):
+    def __init__(self, wallet_swarm: WalletSwarm):
         super().__init__(wallet_swarm)
-        self.transaction_threshold = transaction_threshold
+        self.bot = RevengeBot()
 
-    def identify_opportunities(self, transaction_data: List[Dict[str, float]]) -> List[Dict[str, float]]:
-        return [
-            {'transaction_hash': tx['hash'], 'value': tx['value']}
-            for tx in transaction_data if tx['value'] > self.transaction_threshold
-        ]
+    def identify_opportunities(self, data):
+        return self.bot.fetch_market_data()
 
-    def execute(self, opportunity: Dict[str, float]) -> float:
-        profit = opportunity['value'] * 0.01  # Assume 1% profit
-        return profit
+    def execute(self, opportunity):
+        return self.bot.execute_trade(opportunity)
 
 
-class SandwichAttack(MEVStrategy):
-    def __init__(self, wallet_swarm: WalletSwarm, transaction_threshold: float = 1000):
+class SniperTrading(MEVStrategy):
+    def __init__(self, wallet_swarm: WalletSwarm):
         super().__init__(wallet_swarm)
-        self.transaction_threshold = transaction_threshold
+        self.bot = SniperBot()
 
-    def identify_opportunities(self, transaction_data: List[Dict[str, float]]) -> List[Dict[str, float]]:
-        return [
-            {'transaction_hash': tx['hash'], 'value': tx['value']}
-            for tx in transaction_data if tx['value'] > self.transaction_threshold
-        ]
+    def identify_opportunities(self, data):
+        return self.bot.fetch_market_data()
 
-    def execute(self, opportunity: Dict[str, float]) -> float:
-        profit = opportunity['value'] * 0.02  # Assume 2% profit
-        return profit
+    def execute(self, opportunity):
+        return self.bot.execute_trade(opportunity)
 
 
-# Dictionary of MEV strategies for dynamic usage
+class StatisticalArbitrage(MEVStrategy):
+    def __init__(self, wallet_swarm: WalletSwarm):
+        super().__init__(wallet_swarm)
+        self.bot = StatisticalArbitrageBot()
+
+    def identify_opportunities(self, data):
+        return self.bot.fetch_market_data()
+
+    def execute(self, opportunity):
+        return self.bot.execute_trade(opportunity)
+
+
+class TriangleArbitrage(MEVStrategy):
+    def __init__(self, wallet_swarm: WalletSwarm):
+        super().__init__(wallet_swarm)
+        self.bot = TriangleArbitrageBot()
+
+    def identify_opportunities(self, data):
+        return self.bot.fetch_market_data()
+
+    def execute(self, opportunity):
+        return self.bot.execute_trade(opportunity)
+
+
+class LiquidityDrain(MEVStrategy):
+    def __init__(self, wallet_swarm: WalletSwarm):
+        super().__init__(wallet_swarm)
+        self.bot = LiquidityDrainBot()
+
+    def identify_opportunities(self, data):
+        return self.bot.fetch_market_data()
+
+    def execute(self, opportunity):
+        return self.bot.execute_trade(opportunity)
+
+
+class LiquidityProvision(MEVStrategy):
+    def __init__(self, wallet_swarm: WalletSwarm):
+        super().__init__(wallet_swarm)
+        self.bot = LiquidityProvisionArbitrageBot()
+
+    def identify_opportunities(self, data):
+        return self.bot.fetch_market_data()
+
+    def execute(self, opportunity):
+        return self.bot.execute_trade(opportunity)
+
+
+# Dictionary of all available MEV strategies
 MEV_STRATEGIES = {
     'Arbitrage': Arbitrage,
-    'FrontRunning': FrontRunning,
-    'BackRunning': BackRunning,
-    'SandwichAttack': SandwichAttack
+    'MovingAverageCrossover': MovingAverageCrossover,
+    'RevengeTrading': RevengeTrading,
+    'SniperTrading': SniperTrading,
+    'StatisticalArbitrage': StatisticalArbitrage,
+    'TriangleArbitrage': TriangleArbitrage,
+    'LiquidityDrain': LiquidityDrain,
+    'LiquidityProvision': LiquidityProvision,
 }
