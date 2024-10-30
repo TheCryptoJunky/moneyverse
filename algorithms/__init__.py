a81adedc7568dd7e5a816dcb0178930568aeec92
TheCryptoJunky <thecryptojunky@gmail.com>
2024-10-30 00:42:04 -0400

    Update __init__.py

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -1,42 +1,46 @@
 # algorithms/__init__.py
 
-# Import all strategy bot classes for streamlined access across the project
+# Import all strategy bot classes for access across the project
 from strategies.arbitrage_bot import ArbitrageBot
+from strategies.assassin_bot import AssassinBot
+from strategies.cross_chain_arbitrage import CrossChainArbitrageBot
+from strategies.enhanced_sandwich_attack_bot import EnhancedSandwichAttackBot
+from strategies.flash_loan_arbitrage_bot import FlashLoanArbitrageBot
 from strategies.front_running_bot import FrontRunningBot
 from strategies.latency_arbitrage_bot import LatencyArbitrageBot
-from strategies.enhanced_sandwich_attack_bot import EnhancedSandwichAttackBot
-from strategies.assassin_bot import AssassinBot
+from strategies.liquidity_drain_bot import LiquidityDrainBot
+from strategies.liquidity_provision_arbitrage_bot import LiquidityProvisionArbitrageBot
+from strategies.market_impact_arbitrage_bot import MarketImpactArbitrageBot
 from strategies.market_maker_bot import MarketMakerBot
+from strategies.mev_strategy import MEVStrategy
+from strategies.moving_average_crossover_strategy import MovingAverageCrossoverStrategy
 from strategies.multi_exchange_arbitrage_bot import MultiExchangeArbitrageBot
-from strategies.cross_chain_arbitrage import CrossChainArbitrageBot
+from strategies.protection import ProtectionBot
+from strategies.reinforced_accumulation_bot import ReinforcedAccumulationBot
 from strategies.revenge_bot import RevengeBot
+from strategies.sniper_bot import SniperBot
 from strategies.statistical_arbitrage_bot import StatisticalArbitrageBot
 from strategies.triangle_arbitrage import TriangleArbitrageBot
-from strategies.flash_loan_arbitrage_bot import FlashLoanArbitrageBot
-from strategies.atomic_arbitrage_bot import AtomicArbitrageBot
-from strategies.gas_optimization_bot import GasOptimizationBot
-from strategies.sniper_bot import SniperBot
-from strategies.flash_liquidation_bot import FlashLiquidationBot
-from strategies.latency_arbitrage_bot import LatencyArbitrageBot
-from strategies.reinforced_accumulation_bot import ReinforcedAccumulationBot
 
 __all__ = [
     "ArbitrageBot",
+    "AssassinBot",
+    "CrossChainArbitrageBot",
+    "EnhancedSandwichAttackBot",
+    "FlashLoanArbitrageBot",
     "FrontRunningBot",
     "LatencyArbitrageBot",
-    "EnhancedSandwichAttackBot",
-    "AssassinBot",
+    "LiquidityDrainBot",
+    "LiquidityProvisionArbitrageBot",
+    "MarketImpactArbitrageBot",
     "MarketMakerBot",
+    "MEVStrategy",
+    "MovingAverageCrossoverStrategy",
     "MultiExchangeArbitrageBot",
-    "CrossChainArbitrageBot",
+    "ProtectionBot",
+    "ReinforcedAccumulationBot",
     "RevengeBot",
+    "SniperBot",
     "StatisticalArbitrageBot",
     "TriangleArbitrageBot",
-    "FlashLoanArbitrageBot",
-    "AtomicArbitrageBot",
-    "GasOptimizationBot",
-    "TokenSnipingBot",
-    "FlashLiquidationBot",
-    "ReinforcedAccumulationBot",
-    "SniperBot",
 ]
